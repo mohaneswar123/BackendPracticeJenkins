9e2e8fafeba7d68feb8d53920dab01236be8e62b
mohaneswar123 <pvmohaneswar@gmail.com>
2025-10-13 20:26:54 +0530

    backend commit

--- src/main/java/com/own/practice/OwnbackendjenkinsApplication.java
+++ src/main/java/com/own/practice/OwnbackendjenkinsApplication.java
@@ -2,12 +2,14 @@ package com.own.practice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class OwnbackendjenkinsApplication {
+public class OwnbackendjenkinsApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OwnbackendjenkinsApplication.class, args);
+		System.out.println("runnnnn");
 	}
 
 }
